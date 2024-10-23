938a076a75b8d87fb63064e02eb1891b94a28b55
Jatin <bjatinkaushik@gmail.com>
2024-10-23 22:37:53 +0530

    Centered the skeleton of loading screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,15 +32,17 @@ function App() {
     dispatch(checkAuth(token))
   }, [dispatch]);
 
-  if(isLoading) return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+  if (isLoading) return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center space-y-4">
+        <Skeleton className="h-[150px] w-[300px] rounded-xl bg-gray-200" />
+        <div className="space-y-2">
+          <Skeleton className="h-5 w-[300px] bg-gray-200" />
+          <Skeleton className="h-5 w-[250px] bg-gray-200" />
+        </div>
       </div>
     </div>
-  )
+  );
   
 
   return (
